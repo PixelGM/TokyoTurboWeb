28211820cdd942d9c1002c69cd50ccb2f44ec549
ピクセルGM <35326620+PixelGM@users.noreply.github.com>
2023-11-06 20:01:43 -0800

    Implemented Input and Output on the Website

--- TokyoTurboWeb/TTW/views.py
+++ TokyoTurboWeb/TTW/views.py
@@ -1,8 +1,9 @@
 # TokyoTurboWeb/views.py
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 import math
 
+# Function to calculate the dew point
 def calculate_dew_point(T_ambient, H_ambient):
     a = 17.27
     b = 237.7
@@ -10,25 +11,28 @@ def calculate_dew_point(T_ambient, H_ambient):
     T_dew = (b * alpha) / (a - alpha)
     return T_dew
 
+# Function to detect dew formation
 def dew_formation_detector(T_ambient, T_windshield, H_ambient):
     T_dew = calculate_dew_point(T_ambient, H_ambient)
     return T_windshield <= T_dew
 
+# View to handle dew point detection
 def dew_point_view(request):
-    # Inputs
-    T_ambient = 10.0      # Ambient temperature in degrees Celsius
-    T_windshield = 8.0    # Windshield temperature in degrees Celsius
-    H_ambient = 85.0      # Ambient humidity in percent
+    dew_message = ""
+    if request.method == 'POST':
+        # Get values from POST request
+        T_ambient = float(request.POST.get('ambientTemp', 0))
+        T_windshield = float(request.POST.get('windshieldTemp', 0))
+        H_ambient = float(request.POST.get('humidity', 100))
 
-    # Perform the dew detection
-    dew_detected = dew_formation_detector(T_ambient, T_windshield, H_ambient)
+        # Perform the dew detection
+        dew_detected = dew_formation_detector(T_ambient, T_windshield, H_ambient)
 
-    # Prepare the message
-    dew_message = "Dew formation detected." if dew_detected else "No dew formation detected."
+        # Set the result message
+        dew_message = "Dew formation detected." if dew_detected else "No dew formation detected."
 
-    # Render the index.html template with the dew_message context
+    # Render the HTML template with the dew_message
     return render(request, 'index.html', {'dew_message': dew_message})
-
 
 # def hello_world(request):
 #     return render(request, 'index.html')
